c6d91b9a8999af5cf566aa48a79dfc0a80a291d3
nicolas riera <nicolariera@gmail.com>
2024-05-01 21:34:14 +0200

    added tailwind in project

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.html",
+    // add paths to all other files that use Tailwind classes here
+  ],
+  // rest of the config
+};
