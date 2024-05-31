16e223b92ac8f26c396e3beb07bb6095191e6670
nicolas riera <nicolariera@gmail.com>
2024-05-31 10:40:12 +0200

    added search for appliances dropdown

--- src/scripts/components/DropdownFilterIngredients.js
+++ src/scripts/components/DropdownFilterIngredients.js
@@ -3,6 +3,67 @@ class DropdownFilterIngredients {
     this._dropdown = dropdown;
   }
 
+  onSearchIngredients() {
+    const $inputSearch = document.getElementById("search-ingredients");
+
+    if (!$inputSearch) {
+      console.error("L'élément avec l'ID 'search-ingredients' n'existe pas.");
+      return;
+    }
+
+    let debounceTimeout;
+    $inputSearch.addEventListener("input", (e) => {
+      e.preventDefault();
+      const searchValue = e.target.value.toLowerCase();
+
+      clearTimeout(debounceTimeout);
+      debounceTimeout = setTimeout(() => {
+        this.updateDropdownIngredients(searchValue);
+      }, 300); // Attend 300ms après la dernière frappe de l'utilisateur avant de mettre à jour la liste déroulante
+    });
+  }
+
+  loadAllIngredients() {
+    this.updateDropdownIngredients();
+  }
+
+  updateDropdownIngredients(searchValue = "") {
+    const dropdownValues = Object.values(this._dropdown);
+
+    const dropdownFiltered = dropdownValues.filter((value) => {
+      return value.toLowerCase().includes(searchValue);
+    });
+
+    const dropdownSorted = dropdownFiltered.sort((a, b) => {
+      return a.toLowerCase().localeCompare(b.toLowerCase());
+    });
+
+    const $dropdownIngredientsButtons = (this.$dropdownFiltersIngredients =
+      document.getElementById("dropdown-ingredients-buttons"));
+
+    $dropdownIngredientsButtons.innerHTML = "";
+
+    dropdownSorted.forEach((value) => {
+      const $wrapper = document.createElement("button");
+      $wrapper.classList.add(
+        "w-full",
+        "text-left",
+        "hover:bg-yellow-300",
+        "p-2"
+      );
+
+      const dropdownFilter = `
+          ${value}
+      `;
+
+      $wrapper.innerHTML = dropdownFilter;
+
+      setTimeout(() => {
+        $dropdownIngredientsButtons.appendChild($wrapper);
+      });
+    }, 1000);
+  }
+
   createDropdownFilterIngredients() {
     const $wrapper = document.createElement("button");
     $wrapper.classList.add("w-full", "text-left", "hover:bg-yellow-300", "p-2");
