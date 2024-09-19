b54214fe7387b3988fe640a45f6f867a1cf1d7a3
nicolas riera <nicolariera@gmail.com>
2024-09-19 09:21:55 +0200

    udpated structur project and readme

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 // Fonction pour récupérer les recettes depuis un fichier JSON
 async function fetchRecipes() {
   try {
-    const response = await fetch("src/data/recipes.json");
+    const response = await fetch("src/staticData/recipes.json");
     const data = await response.json();
     return data.recipes;
   } catch (error) {
