997df9a18c20e52592698f448220cc82b2a6ef87
nicolas riera <nicolariera@gmail.com>
2024-09-25 21:28:24 +0200

    added file for cache in netlify

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,16 @@
-// Fonction pour récupérer les recettes depuis un fichier JSON
+let cachedRecipes = null;
+
 async function fetchRecipes() {
+  if (cachedRecipes) {
+    console.log("Utilisation des recettes en cache");
+    return cachedRecipes;
+  }
+
   try {
     const response = await fetch("src/staticData/recipes.json");
     const data = await response.json();
-    return data.recipes;
+    cachedRecipes = data.recipes; // Mettre en cache les recettes
+    return cachedRecipes;
   } catch (error) {
     console.error("Erreur lors de la récupération des recettes :", error);
     return [];
